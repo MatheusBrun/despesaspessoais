45ce217e0fa25a7b75f425dc8756d6e837fcaba6
Matheus_bruno <mte.bruno1@gmail.com>
2022-06-07 11:20:59 -0300

    mudando o nome

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'hello word',
+      title: 'Mudando o nome',
       theme: ThemeData(
         // This is the theme of your application.
         //
